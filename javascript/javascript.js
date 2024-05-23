e5cdefc27373122187dc057be4bf307c8875b263
sinajr <jamyarisina@gmail.com>
2024-05-24 01:41:24 +0300

    update

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -23,12 +23,14 @@ let foodArray=[
 let orderQnty=$.getElementById('order-qnty');
 let showFoodMenu=$.getElementById('content-menu');
 let cardShow=$.getElementById('card-show')
+let totalPrice=$.querySelector('#total-price');
 let ordersArray=[]
 function foodMenuGenerator(foodarr){
     foodarr.forEach(function(foods){
         showFoodMenu.insertAdjacentHTML('beforeend','<div class="col-md-3"><div class="card" style="width: 100%;"><img src="'+foods.imageUrl+'"class="w-100 card-img-top" alt="..."><div class="card-body text-center"><h5 class="card-title">'+foods.foodName+'</h5><p class="card-text"><span>'+foods.price+'</span><span>$</span></p><a href="#" class="add-card btn btn-dark">Add to card</a></div></div></div>')
     })
     let addBtn=$.querySelectorAll('.add-card');
+    let sum=0;
     function addFoodToCard(addBtn){
         let qnty=0;
         addBtn.forEach(function(add){
@@ -42,9 +44,11 @@ function foodMenuGenerator(foodarr){
                     orderImage:foodImgOrder
                 }
                 ordersArray.push(arrayFororders);
-                console.log(ordersArray)
                 cardShow.insertAdjacentHTML('beforeend','<div class="row my-3 d-flex justify-content-center"><div class="text-center col-md-4 align-self-center"><img style="border-radius: 10px;" src="'+ordersArray[qnty].orderImage+'" width="100" height="100" alt=""></div><div class="text-center col-md-4 align-self-center">'+ordersArray[qnty].orderName+'</div><div class="text-center col-md-4 align-self-center"><span>'+ordersArray[qnty].orderPrice+'</span><span>$</span></div></div>')
-                console.log(ordersArray)
+                let firstNum=ordersArray[qnty].orderPrice;
+                sum=sum+firstNum;
+                totalPrice.innerHTML=sum
+
                 qnty++
                 orderQnty.innerHTML=qnty
             })
